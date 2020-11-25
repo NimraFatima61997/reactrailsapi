00bfac6cbceb6992f5aa99e0e3187fd04cb5e84a
NimraFatima61997 <bsef16m533@pucit.edu.pk>
2020-11-25 19:11:10 +0500

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   mount_devise_token_auth_for 'User', at: 'auth'
   resources :campaigns 
   resources :comments
@@ -10,6 +12,6 @@ Rails.application.routes.draw do
     end
   end
   resources :discussiontopics
-
+  root :to => 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
